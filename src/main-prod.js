fb26763085940338545d1156e20a776277285411
JiqiangZhu <jiqiang.zhu01@gmail.com>
2022-01-18 22:12:32 +0800

    'CI'

--- src/main-prod.js
+++ src/main-prod.js
@@ -7,6 +7,6 @@ import './common/rem.js'
 import { Toast, Popup } from 'vant';
 
 window.Toast = Toast
-console.log('测试 CI/CD')
+console.log('测试 CI/CD 1')
 createApp(App).use(store).use(router).use(Popup).mount('#app')
 
